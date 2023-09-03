88f2e76295c3998badf3a1fc8fc726cf8f5553fa
jovanny-macbookair <jovannygochez23@gmail.com>
2023-09-03 00:41:42 -0500

    Create POS system for a startup lemonade stand.

--- LemonadeStandPosSystem.py
+++ LemonadeStandPosSystem.py
@@ -5,68 +5,86 @@
 
 class InvalidSalesItemError(Exception):
     """Error function when item sold is not on menu"""
+
     def __init__(self, value):
         self.value = value
 
 
 class MenuItem:
     """ Creates menu object with name, wholesale- and selling price"""
+
     def __init__(self, name, wholesale_cost, selling_price):
-        ' Create data members from parameters'
+        """Create data members from parameters"""
         self._item_name = name
-        self._wholesale_cost = wholesale_cost # float
-        self._selling_price = selling_price # float
+        self._wholesale_cost = wholesale_cost  # float
+        self._selling_price = selling_price  # float
+
     def get_name(self):
+        """ return name"""
         return self._item_name
+
     def get_wholesale_cost(self):
-        return self._wholesale_cost # float
+        """return wholesale cost"""
+        return self._wholesale_cost  # float
+
     def get_selling_price(self):
-        return self._selling_price # float
+        """return selling price"""
+        return self._selling_price  # float
 
 
 class SalesForDay:
     """Retrieve history on sales and corresponding day"""
-    def __init__(self, num_days, sales_ldictionary): # item_for_day = {item: num_sold}
-        "Set up objects of how long stand has been open and its sale record"
-        self._number_of_days_since_open = num_days # integer
-        self._item_sold_on_specific_day = sales_ldictionary # item_for_day = {item: num_sold}
+
+    def __init__(self, num_days, sales_ldictionary):  # item_for_day = {item: num_sold}
+        """Set up objects of how long stand has been open and its sale record"""
+        self._number_of_days_since_open = num_days  # integer
+        self._item_sold_on_specific_day = sales_ldictionary  # item_for_day = {item: num_sold}
 
     def get_day(self):
-        return self._number_of_days_since_open # integer
+        """return number of days since open"""
+        return self._number_of_days_since_open  # integer
+
     def get_sales(self):
-        return self._item_sold_on_specific_day # item_for_day = {item: num_sold}
+        """return number of items sold on a specific day"""
+        return self._item_sold_on_specific_day  # item_for_day = {item: num_sold}
 
 
 class LemonadeStand:
     """Main class where program runs the menu, sales, and profit
      that will be verified, organized, and recorded"""
-    def __init__(self,name):
-        "Main sources of info of stand will be here including what day it is,"
-        "the current menu, and current selling history"
+
+    def __init__(self, name):
+        """Main sources of info of stand will be here including what day it is,
+        the current menu, and current selling history"""
 
         # Create new slate of objects to be used when menu and sales are added
         self._stand_name = name
         current_day = 0
         self._current_day = current_day
-        item_and_price = {} # { item : price}
+        item_and_price = {}  # { item : price}
         self._menu_item_and_price = item_and_price
-        sales_for_day = [] # [{day : {item : num_sold}}]
+        sales_for_day = []  # [{day : {item : num_sold}}]
         self._sale_for_day = sales_for_day
 
     def get_name(self):
+        """return stand name"""
         return self._stand_name
+
     def add_menu_item(self, new_item_and_price):
+        """add new item and price"""
         self._menu_item_and_price.update(new_item_and_price)
         return self._menu_item_and_price
 
     def get_price(self, name):
+        """return price for given item"""
         for item in self._menu_item_and_price:
             if name in item:
                 item_price = self._menu_item_and_price[item]
 
         return item_price
 
-    def enter_sales_for_today(self, new_sale): # new_sale = {item_name : num_sold}
+    def enter_sales_for_today(self, new_sale):  # new_sale = {item_name : num_sold}
+        """enter sale for a given day"""
 
         try:
             # First verify sale matches menu, then we will copy our sale onto empty object
@@ -93,12 +111,12 @@ class LemonadeStand:
         return self._sale_for_day
 
     def sales_of_menu_item_for_day(self, number_of_day, name_of_item):
-        # Purpose is to find quantity of item sold on specific day
-        # Call methods in SalesForDay class to extract desired information
+        """Purpose is to find quantity of item sold on specific day
+        Call methods in SalesForDay class to extract desired information"""
 
         specific_sale = SalesForDay(self._current_day, self._sale_for_day)
         day = specific_sale.get_day()
-        record = specific_sale.get_sales() # list of dict
+        record = specific_sale.get_sales()  # list of dict
 
         # Verify that day requested is valid by comparing to days since stand
         # was opened
@@ -114,9 +132,9 @@ class LemonadeStand:
         else:
             print("Stand hasn't been open for that long!")
 
-    def total_sales_for_menu_item(self,name):
-        # purpose is to find quantity of item sold there entire stand
-        # has been in operation
+    def total_sales_for_menu_item(self, name):
+        """purpose is to find quantity of item sold there entire stand
+        has been in operation"""
 
         # Call SalesForDay class and methods to find days open and total history of sales
         specific_sale = SalesForDay(self._current_day, self._sale_for_day)
@@ -126,7 +144,7 @@ class LemonadeStand:
         # Iterate over every day the stand has been open to find record of sales
         # If the item is found, the var quantity_count is increased by quantity of sales
         quantity_count = 0
-        for i in range(0,day):
+        for i in range(0, day):
             for obj in record[i]:
                 if name in obj:
                     quantity_count += (record[i][name])
@@ -135,7 +153,7 @@ class LemonadeStand:
         return quantity_count
 
     def total_profit_for_menu_item(self, name):
-        # Extract entire sales history and revenue based off name of item
+        """Extract entire sales history and revenue based off name of item"""
         number_sold = self.total_sales_for_menu_item(name)
         selling_price = self.get_price(name)
         wholesale_price = selling_price / 2
@@ -145,15 +163,13 @@ class LemonadeStand:
         return profit_for_item
 
     def total_profit_for_stand(self):
-        # Iterate over entire menu and return profit for each item and add them
-        # onto the variable total_profit
+        """Iterate over entire menu and return profit for each item and add them
+        onto the variable total_profit"""
         total_profit = 0
         for obj in self._menu_item_and_price:
             total_profit += self.total_profit_for_menu_item(obj)
 
         return float(total_profit)
-
-
 
 
 def main():
@@ -178,7 +194,7 @@ def main():
     # Check on sale of an item on a specific day
     day_n = 1
     item_search = "strawberry"
-    item_sales = stand.sales_of_menu_item_for_day( day_n, item_search)
+    item_sales = stand.sales_of_menu_item_for_day(day_n, item_search)
     print(f"On day '{day_n}', the number of times "
           f"the item '{item_search}' was sold was: '{item_sales}' times!")
 
